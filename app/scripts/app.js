7230ef903d8ee0957f80796eeffeb1774856844a
JackyShen <mebusw@163.com>
2015-03-10 23:29:20 +0800

    use plugin to detect network connection

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global StatusBar */
+/* global StatusBar, $, Connection */
 
 /**
  * @ngdoc overview
@@ -9,45 +9,66 @@
  *
  * Main module of the application.
  */
-angular
-  .module('yoApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.sortable',
-    'LocalStorageModule'
+ angular
+ .module('yoApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'ui.sortable',
+  'LocalStorageModule'
   ])
-  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
-    localStorageServiceProvider.setPrefix('ls');
-  }])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+ .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+  localStorageServiceProvider.setPrefix('ls');
+}])
+ .config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl'
+  })
+  .when('/about', {
+    templateUrl: 'views/about.html',
+    controller: 'AboutCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
+});
 
+
+    function checkConnection() {
+      // plugin org.apache.cordova.network-information
+      if (navigator.connection !== undefined) {
+        var networkState = navigator.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        $('$conn_type').text('Connection type: ' + states[networkState]);
+      }
+    }
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
 
-document.addEventListener('deviceready', function() {
+    document.addEventListener('deviceready', function() {
       console.warn('deviceready');
       StatusBar.overlaysWebView( false );
       StatusBar.backgroundColorByName( 'gray' );
-}, false);
 
-document.addEventListener('offline', function() {
+      checkConnection();
+    }, false);
+
+    document.addEventListener('offline', function() {
       console.warn('offline');
-}, false);
+    }, false);
 
